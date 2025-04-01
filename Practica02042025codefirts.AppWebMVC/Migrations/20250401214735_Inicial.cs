ce0086f9645da30c16abbb88345fde205014c1d9
Jazmin <ACER@LAPTOP-FRMPB979>
2025-04-02 10:34:15 +1245

    cambio

---
+++ Practica02042025codefirts.AppWebMVC/Migrations/20250401214735_Inicial.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Practica02042025codefirts.AppWebMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Alumnos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<int>(type: "int", nullable: false),
+                    Codigo = table.Column<int>(type: "int", nullable: false),
+                    Direccion = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<int>(type: "int", nullable: false),
+                    FechaNacimiento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AñoEstudio = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alumnos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Empleados",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<int>(type: "int", nullable: false),
+                    Telefono = table.Column<int>(type: "int", nullable: false),
+                    DUI = table.Column<int>(type: "int", nullable: false),
+                    Direccion = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Empleados", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Alumnos");
+
+            migrationBuilder.DropTable(
+                name: "Empleados");
+        }
+    }
+}
---
+++ Practica02042025codefirts.AppWebMVC/Models/ESFEDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Practica02042025codefirts.AppWebMVC.Models
+{
+    public class ESFEDbContext :DbContext
+    {
+        public ESFEDbContext(DbContextOptions<ESFEDbContext> options) : base(options) { }
+        public DbSet<Empleado> Empleados { get; set; }
+        public DbSet<Alumno> Alumnos { get; set; }
+
+    }
+}
